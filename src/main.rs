7df434631a8866d378f6a29f549fec5248fab39f
garebare <garedaiki@gmail.com>
2021-06-28 11:40:18 +0900

    vga text mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World";
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    loop {}
 }
