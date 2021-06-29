95f5a8e5c3f88832c76f4b28c5269e717f5ca020
garebare <garedaiki@gmail.com>
2021-06-29 15:36:25 +0900

    Double Foult

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,18 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+pub mod gdt;
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
-pub mod interrupts;
-pub mod gdt;
 
 use core::panic::PanicInfo;
 
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,23 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
 use blog_os::println;
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  println!("Hello World{}", "!");
+    println!("Hello World{}", "!");
 
-  blog_os::init();
-  x86_64::instructions::interrupts::int3(); 
+    blog_os::init();
 
-  #[cfg(test)]
-  test_main();
+    #[cfg(test)]
+    test_main();
 
-  println!("It did not crash!");
-  loop {}
+    println!("It did not crash!");
+    loop {
+        use blog_os::print;
+        print!("-");
+    }
 }
 
 #[panic_handler]
